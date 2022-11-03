09a81ec2ee10ca577ab6324d8d4427652fe50f03
Katya <kittyserenakat@gmail.com>
2022-11-03 16:35:17 -0400

    Set up form input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
+import React from 'react';
+
 import './App.css';
+import ParticlesBg from 'particles-bg'
+
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import ParticlesBg from 'particles-bg'
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-      <ParticlesBg className='particles' type='cobweb' color='#ffffff' num={100} bg={true} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+  render(){
+    return (
+      <div className="App">
+        <ParticlesBg 
+          className='particles' 
+          type='cobweb' 
+          color='#ffffff' 
+          num={100} 
+          bg={true} 
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    )
+  }
 }
 
 export default App;
