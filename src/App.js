6c725bc6913eb86ae3fbbd17bf319dca1863fac7
Katya <kittyserenakat@gmail.com>
2022-11-03 22:43:55 -0400

    Add Clarifai API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import React from 'react';
 
 import './App.css';
 import ParticlesBg from 'particles-bg'
+// import Clarifai from 'clarifai';
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+// import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+
+const USER_ID = 'adhdcoder';
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = 'bf604ccf0cc54ae8a91711967c79b462';
+const APP_ID = 'facerecognitionbrain';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+const raw = JSON.stringify({
+  "user_app_id": {
+    "user_id": USER_ID,
+    "app_id": APP_ID
+  },
+  "inputs": [
+    {
+      "data": {
+        "image": {
+          "url": IMAGE_URL
+        }
+      }
+    }
+  ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
 
 class App extends React.Component {
   constructor(){
@@ -20,7 +55,10 @@ class App extends React.Component {
     console.log(event.target.value);
   }
   onButtonSubmit = () => {
-    console.log('click')
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
   render(){
     return (
