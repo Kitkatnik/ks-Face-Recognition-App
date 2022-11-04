fdb91e4f01010bd7bf597a0ba277d1205477698d
Katya <kittyserenakat@gmail.com>
2022-11-04 00:54:29 -0400

    Add image recognition

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-// import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
 const USER_ID = 'adhdcoder';
 // Your PAT (Personal Access Token) can be found in the portal under Authentification
@@ -17,47 +17,45 @@ const APP_ID = 'facerecognitionbrain';
 // Change these to whatever model and image URL you want to use
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
-
-const raw = JSON.stringify({
-  "user_app_id": {
-    "user_id": USER_ID,
-    "app_id": APP_ID
-  },
-  "inputs": [
-    {
-      "data": {
-        "image": {
-          "url": IMAGE_URL
-        }
-      }
-    }
-  ]
-});
-
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Authorization': 'Key ' + PAT
-  },
-  body: raw
-};
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
   onButtonSubmit = () => {
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
+    this.setState({imageUrl: this.state.input})
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: JSON.stringify({
+        "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+        },
+        "inputs": [
+          {
+            "data": {
+              "image": {
+                "url": this.state.input 
+              }
+            }
+          }
+        ]
+      })
+    })
+      .then(response => response.json()) 
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
       .catch(error => console.log('error', error));
   }
   render(){
@@ -77,7 +75,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit} 
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
   }
